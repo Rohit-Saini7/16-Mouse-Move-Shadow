f9340a3a1bc336b44bf8a099e17deb6cfaccba93
Rohit-Saini7 <rohit712wd@gmail.com>
2022-07-18 14:48:44 +0530

    add screenshot

--- js/index.js
+++ js/index.js
@@ -13,10 +13,10 @@ function shadow(e) {
   const yWalk = Math.round((y / height) * walk - walk / 2);
 
   text.style.textShadow = `
-      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
-      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
-      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
-      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)`;
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.5),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.5),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.5),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.5)`;
 }
 
 hero.addEventListener("mousemove", shadow);
